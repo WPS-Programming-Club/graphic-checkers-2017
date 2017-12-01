f2a31a65920109e9a127a969729c9810897b93e1
EW2000 <33058189+Wanganator414@users.noreply.github.com>
2017-11-30 21:50:57 -0500

    Update ericcshwtemporary.java

--- ericcshwtemporary.java
+++ ericcshwtemporary.java
@@ -138,3 +138,70 @@ public class Movie {
 
 
 
+
+
+
+
+
+import java.util.*;
+public class Movie {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello, please tell me your age: ");
+		Scanner kb = new Scanner (System.in);
+		int aje = kb.nextInt();
+	
+		System.out.println("Are you here for a matinee showing?     ('Y' or 'N')");
+		Scanner kb2 = new Scanner (System.in);
+		String x = kb2.nextLine();
+
+		System.out.println("Are you here for a regular showing?     ('Y' or 'N')");
+		Scanner kb3 = new Scanner (System.in);
+		String y = kb3.nextLine();
+		
+	
+		System.out.println("Let me see.....");
+		
+	
+		Movieinfo steve = new Movieinfo(aje, x, y); 		//First parameter is for age, second a Y or N for matinee, and the third a Y or N for regular.
+		String movietype = null;
+	
+		int decision = 5;
+		
+		if (steve.getAge() < 10 && steve.getCheck1() == "Y" && steve.getCheck2() == "N"){
+			    
+				  movietype = "matinee";
+				  decision = 2;
+				  System.out.println ("Your age is " + steve.getAge() + ", you are here for a " + movietype + " showing " + "and your cost would be " + decision +" dollars.");
+				 
+				}
+		else if (steve.getAge() > 55 && steve.getCheck2() == "Y" && steve.getCheck1() == "N") {
+			
+				   movietype = "regular";
+				   decision = 7;
+				   System.out.println ("Your age is " + steve.getAge() + ", you are here for a " + movietype + " showing " + "and your cost would be " + decision +" dollars.");
+				}
+		else if  (steve.getAge() > 10 && steve.getCheck1() == "Y" && steve.getCheck2() == "N"){ 	
+					movietype = "matinee";
+					decision = 5;
+					System.out.println ("Your age is " + steve.getAge() + ", you are here for a " + movietype + " showing " + "and your cost would be " + decision +" dollars.");
+					}	
+		else if (steve.getAge() < 55 && steve.getCheck2() == "Y" && steve.getCheck1() == "N") {
+					movietype = "regular"; 
+					decision = 10;
+					System.out.println ("Your age is " + steve.getAge() + ", you are here for a " + movietype + " showing " + "and your cost would be " + decision +" dollars.");
+					}
+		else {}
+				}
+		} 
+			
+				
+	
+
+
+
+
+
+
